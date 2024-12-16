9ac2d0edf5ab8f7768c584fbd21f508dd137f6a6
mkalino1 <mateuszkalin1@gmail.com>
2024-12-16 22:02:39 +0100

    :recycle: Refactor first puzzle to be more functional

--- day-05/puzzle1.ts
+++ day-05/puzzle1.ts
@@ -3,12 +3,8 @@ import { rules, updates } from "./dataLoad";
 
 const rulesMap = generateRulesMap(rules);
 
-let sum = 0;
-
-updates.forEach((update: number[]) => {
-  sum +=
-    update[Math.floor(update.length / 2)] *
-    Number(!isUpdateInvalid(update, rulesMap));
-});
+const sum = updates
+  .filter((update) => !isUpdateInvalid(update, rulesMap))
+  .reduce((sum, update) => sum + update[Math.floor(update.length / 2)], 0);
 
 console.log(sum);
