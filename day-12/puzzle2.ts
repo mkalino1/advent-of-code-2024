1aabaee5d0023ef44321a8f6cc6f1d086b61637c
mkalino1 <mateuszkalin1@gmail.com>
2025-02-22 14:11:11 +0100

    :recycle: Refactor day 12 for cleaner code

--- day-12/puzzle2.ts
+++ day-12/puzzle2.ts
@@ -4,17 +4,15 @@ type Plot = {
   x: number,
   y: number,
   plant: string,
-  regionId: number,
   validNeighbours: Plot[],
   borders: Set<Border>,
+  isTraversed: boolean
 }
 
 type Border = 'up' | 'right' | 'down' | 'left'
 
 type Region = {
-  area: number,
   sides: number,
-  plant: string,
   plots: Plot[]
 }
 
@@ -27,19 +25,16 @@ type Scanner = {
 
 const neighboursVectors = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 const directions: Border[] = ['up', 'right', 'down', 'left']
-const regions = new Map<number, Region>()
-let nextRegionId = 0
-
+const regions: Region[] = []
 
 // Prepare matrix of plot objects
 const plotsMatrix: Plot[][] = inputMatrix.map((inputRow, y) =>
   inputRow.map((plant, x) => (
-    { x, y, plant, regionId: -1, validNeighbours: [], borders: new Set() }
+    { x, y, plant, validNeighbours: [], borders: new Set(), isTraversed: false }
   ))
 )
 
-
-// Prepare neighbourhood and borders data for plot objects
+// Prepare neighbours and borders data for plot objects
 plotsMatrix.flat().forEach(plot => {
   neighboursVectors
     .map(vector => plotsMatrix[plot.y + vector[0]]?.[plot.x + vector[1]])
@@ -53,37 +48,28 @@ plotsMatrix.flat().forEach(plot => {
     })
 })
 
-
 // Prepare regions data by traversing recursively
-function traverseRegionRecursively(plot: Plot) {
-  plot.validNeighbours.forEach(neighbour => {
-    // Skip if already visited
-    if (neighbour.regionId == -1) {
-      regions.get(plot.regionId)!.plots.push(neighbour)
-      neighbour.regionId = plot.regionId
-      traverseRegionRecursively(neighbour)
-    }
-  })
-}
+function traverseRegionRecursively(plot: Plot, region: Region | null) {
+  if (plot.isTraversed) return
+  plot.isTraversed = true
 
-plotsMatrix.flat().forEach(plot => {
-  // Traverse from plots with no regionId assigned
-  if (plot.regionId == -1) {
-    // Initialize region
-    plot.regionId = nextRegionId++
-    regions.set(plot.regionId, { area: 0, sides: 0, plant: plot.plant, plots: [] })
-    regions.get(plot.regionId)!.plots.push(plot)
-    traverseRegionRecursively(plot)
+  if (region == null) {
+    const newRegion: Region = { plots: [], sides: 0 }
+    regions.push(newRegion)
+    region = newRegion
   }
-})
+  region.plots.push(plot)
 
+  plot.validNeighbours.forEach(neighbour => traverseRegionRecursively(neighbour, region))
+}
+plotsMatrix.flat().forEach(plot => { traverseRegionRecursively(plot, null) })
 
-// Scan region to count sides
-function scanRegion(region: Region, params: Scanner) {
-  Array.from(params.range).map(scannedAxis => {
+// Scan region to count its sides
+function scanRegion(region: Region, scanner: Scanner) {
+  scanner.range.map(currentPrimaryAxis => {
     const detectedBorders: number[] = region.plots
-      .filter(plot => plot[params.primaryAxis] == scannedAxis && plot.borders.has(params.border))
-      .map(plot => plot[params.secondaryAxis])
+      .filter(plot => plot[scanner.primaryAxis] == currentPrimaryAxis && plot.borders.has(scanner.border))
+      .map(plot => plot[scanner.secondaryAxis])
       .sort((a, b) => a - b)
 
     region.sides += calculateNumberOfSides(detectedBorders)
@@ -98,22 +84,23 @@ function calculateNumberOfSides(bordersOnAxis: number[]) {
   }, []).length
 }
 
-[...regions.values()].forEach(region => {
+regions.forEach(region => {
   // Bounding box of region
   const rangeY = Array.from(region.plots.map(plot => plot.y).reduce((range, curr) => range.add(curr), new Set())) as number[]
   const rangeX = Array.from(region.plots.map(plot => plot.x).reduce((range, curr) => range.add(curr), new Set())) as number[]
 
-  // Scanners for each direction
+  // Scanners for each border direction
   const scanners: Scanner[] = [
     { range: rangeY, primaryAxis: 'y', secondaryAxis: 'x', border: 'up' },
     { range: rangeY, primaryAxis: 'y', secondaryAxis: 'x', border: 'down' },
     { range: rangeX, primaryAxis: 'x', secondaryAxis: 'y', border: 'right' },
     { range: rangeX, primaryAxis: 'x', secondaryAxis: 'y', border: 'left' },
   ]
-  
   scanners.forEach(scanner => scanRegion(region, scanner))
 })
 
-let sum = 0
-regions.forEach(region => sum += region.plots.length * region.sides)
+const sum = regions
+  .map(region => region.plots.length * region.sides)
+  .reduce((sum, curr) => sum + curr)
+
 console.log(sum)
